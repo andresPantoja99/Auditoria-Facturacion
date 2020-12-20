19ce812ee79335702681319432561e4212720b0f
ANDRES SEBASTIAN PANTOJA PINO <andres.pantoja@epn.edu.ec>
2020-12-20 01:17:07 -0500

    archivos iniciales

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using sistema_de_facturacion.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistema_de_facturacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (new Validacion().caracteres(textBox1))
+            {
+                MessageBox.Show("No se permiten caracteres especiales");
+                e.Handled = true;
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("OK");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using sistema_de_facturacion.Principal;
+using sistema_de_facturacion.Login;
+namespace sistema_de_facturacion
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new IngresoAlSistema());
+        }
+    }
+}
